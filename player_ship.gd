e168c574dfd4dcb4daf8ca42b8a656fca316b082
agata <agata.piasecka@outlook.com>
2024-03-17 19:50:49 +0100

    ship animation

--- player_ship.gd
+++ player_ship.gd
@@ -1,17 +1,24 @@
 extends RigidBody2D
 
-@export var thrust = Vector2(0, -350)
-var torque = 10000
+@export var thrust = Vector2(0, -50)
+var torque = 200
 
 func _integrate_forces(state):
 	if Input.is_action_pressed("ui_up"):
 		print("up pressed")
 		state.apply_force(thrust.rotated(rotation))
-	#else:
+		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.animation = "go"
+	else:
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.animation = "default"
 		#state.apply_force(Vector2())
+		
 	var rotation_direction = 0
 	if Input.is_action_pressed("ui_right"):
 		rotation_direction += 1
 	if Input.is_action_pressed("ui_left"):
 		rotation_direction -= 1
 	state.apply_torque(rotation_direction * torque)
+
+
