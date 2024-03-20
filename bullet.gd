b037eb8204da146fa02f0a12d78d8e35e73f263b
przemek06 <swiatprzemyslaw@gmail.com>
2024-03-20 20:36:14 +0100

    laser fix

--- bullet.gd
+++ bullet.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
-var thrust_vector = Vector2(0, -350)
+var thrust_vector = Vector2(0, -700)
 @onready var spaceship: RigidBody2D = get_node("../Spaceship")
+var vector
+var rot
 
 func _ready():
 	var timer = Timer.new()
@@ -9,11 +11,13 @@ func _ready():
 	timer.connect("timeout", queue_free)
 	timer.set_wait_time(5)
 	timer.start()
+	rot = spaceship.global_transform.get_rotation()
+	vector = spaceship.linear_velocity
+	
 
 func _physics_process(delta):
-	var vector = spaceship.global_transform.origin
-	var rotation = spaceship.global_transform.get_rotation()
-	position += thrust_vector.rotated(rotation) * delta
+	position += (thrust_vector.rotated(rot) + vector) * delta
+	
 
 func _on_body_entered(body):
 	if body.is_in_group("Scout"):
