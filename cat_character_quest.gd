ece11b2a1865d31b204b1de6ff1209af4fb5b971
przemek06 <swiatprzemyslaw@gmail.com>
2024-04-24 21:52:56 +0200

    level design

--- cat_character_quest.gd
+++ cat_character_quest.gd
@@ -6,13 +6,13 @@ var interacting = false
 var wasSpaceshipNear = false
 var changed = false
 var status = 'new'
-var spaceship 
+var spaceship
 var dialog
 
 const quest_name = 'Zaginiona ryba'
-const quest_dialog = "Kosmiczny podróżniku! Potrzebuję twojej pomocy - zgubiłem swoją rybę. Proszę pomóż mi ją odnaleźć. Kieruj się ku najbliższej mgławicy a na pewno jej nie przegapisz. \n\n[Naciśnij K, żeby zaakceptować]\n[Naciśnij R, żeby odrzucić]"
+const quest_dialog = "Kosmiczny podróżniku! Potrzebuję twojej pomocy - zgubiłem swoją rybę. Proszę pomóż mi ją odnaleźć, a powiem Ci jak uciec z tego przeklętego układu gwiezdnego. Kieruj się ku najbliższej mgławicy drogą pomiędzy asteroidami a znajdziesz Niebieską Planetę. Moja ryba jest w pobliżu. Ale uważaj! Wrogowie czają się za asteroidami. \n\n[Naciśnij K, żeby zaakceptować]\n[Naciśnij R, żeby odrzucić]"
 const quest_ongoing = "Nadal czekam na moją rybę! \n\n[Naciśnij K, żeby wyjść]"
-const quest_success = "Dziękuję za pomoc! \n\n[Naciśnij K, żeby wyjść]"
+const quest_success = "Dziękuję za pomoc! Kieruj się za dziwnymi asteroidami, a znajdziesz drogę do kolejnego układu. Powodzenia! \n\n[Naciśnij K, żeby wyjść]"
 const quest_after = "Dzień dobry. \n\n[Naciśnij K, żeby wyjść]"
 
 func _ready():
@@ -21,14 +21,14 @@ func _ready():
 	spaceship = get_node('../Spaceship')
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
-func _process(delta):	
+func _process(delta):
 	if isSpaceshipNear and not interacting:
 		interactionAlert.visible = true
 	elif wasSpaceshipNear and (!isSpaceshipNear or interacting):
 		interactionAlert.visible = false
 	if (isSpaceshipNear && Input.is_action_just_pressed("interaction")):
 		_start_interaction()
-	
+
 	wasSpaceshipNear = isSpaceshipNear
 
 	if interacting and not changed:
@@ -43,7 +43,7 @@ func _process(delta):
 		else:
 			_set_dialog(quest_after)
 		changed = true
-	
+
 	if interacting:
 		if status == 'new' and Input.is_action_just_pressed("accept"):
 			status = 'ongoing'
@@ -52,7 +52,7 @@ func _process(delta):
 			_leave_interaction()
 		elif Input.is_action_just_pressed("accept"):
 			_leave_interaction()
-	
+
 func _set_dialog(str):
 	dialog.visible = true
 	dialog.get_child(0).text = str
