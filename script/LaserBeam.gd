6c3b9a4f12bd1a952b30ab426b4fa2f21ce65c28
przemek06 <swiatprzemyslaw@gmail.com>
2024-06-05 20:30:18 +0200

    boss

---
+++ script/LaserBeam.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var target
+var body_inside = false
+var damage_rate = 20
+
+func _ready():
+	$LaserAnimatedSprite2D.animation = "default"
+	$LaserAnimatedSprite2D.play()
+	
+func _process(delta):
+	if not $LaserSound.playing and self.visible:
+		$LaserSound.play(0.0)
+	if not self.visible:
+		$LaserSound.playing = false
+		
+	if target == null:
+		target = get_tree().get_nodes_in_group("Spaceship")[0]
+	if body_inside:
+		target.get_damage(20*delta)
+		
+func _on_body_entered(body: Node):
+	if body == target:
+		body_inside = true
+
+func _on_body_exited(body: Node):
+	if body == target:
+		body_inside = false 
+		
+func _on_laser_sound_finished():
+	if self.visible:
+		$LaserSound.play(0.0)
